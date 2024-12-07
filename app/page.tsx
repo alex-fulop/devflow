5ed765644a5067734480a727689a8f3141c7c0d1
alex-fulop <fulopalexbusiness@gmail.com>
2024-12-07 14:04:38 +0200

    added theme config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-purple-700 font-black">Welcome to the world of Next.js</h1>
+    <h1 className="text-3xl text-light-500 font-black">
+      Welcome to the world of Next.js
+    </h1>
   );
-}
+};
 
 export default Home;
